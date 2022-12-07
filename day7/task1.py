a0c53e34b0e61d9551f12e949646ecc62c575489
Marko Vušak <marko.vusak2@gmail.com>
2022-12-07 20:12:26 +0100

    Add day 7

---
+++ day7/task1.py
@@ -0,0 +1,62 @@
+f = open("input.txt", "r")
+input = f.read().split('\n')
+
+class Directory:
+    def __init__(self, name, parent=None):
+        self.name = name;
+        self.parent = parent;
+        self.files = [];
+        self.subDirs = [];
+
+    def size(self):
+        filesSize = sum(self.files);
+
+        dirsSize = 0;
+        for subDir in self.subDirs:
+            dirsSize += subDir.size();
+        
+        return filesSize + dirsSize;
+    
+    def findDir(self, name):
+        for subDir in self.subDirs:
+            if subDir.name == name:
+                return subDir;
+        return None;
+
+directories = [];
+
+
+currentDir = None;
+
+for cmd in input:
+    if cmd.startswith("dir") or cmd.startswith("$ ls"):
+        continue
+    elif cmd.startswith("$ cd"):
+        parts = cmd.split(' ');
+
+        if currentDir == None:
+            currentDir = Directory('/');
+            directories.append(currentDir);
+
+        if parts[2] == "..":
+            currentDir = currentDir.parent;
+        else:
+            lookedDir = currentDir.findDir(parts[2]);
+
+            if lookedDir == None:
+                newDir = Directory(parts[2], currentDir);
+                currentDir.subDirs.append(newDir);
+                directories.append(newDir);
+
+                currentDir = newDir;
+            else:
+                currentDir = lookedDir;
+    else:
+        currentDir.files.append(int(cmd.split(' ')[0]));
+
+totalSize = 0;
+for dir in directories:
+    if (dir.size() < 100000):
+        totalSize += dir.size();
+
+print(totalSize);
